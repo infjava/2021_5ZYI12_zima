9f1a0350ae8f3fa83a751ae81a374591f16faf3a
Jan Janech <jan.janech@kst.uniza.sk>
2021-10-27 09:14:14 +0200

    04a pridane triedy pre tvaryv3 do projektu pong

---
+++ pong/Lopta.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Lopta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lopta {
+    /**
+     * Constructor for objects of class Lopta
+     */
+    public Lopta() {
+        // initialise instance variables
+    }
+}

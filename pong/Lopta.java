e244e600c588fb6a23f8a31df5bd19ecdbfc6865
Jan Janech <jan.janech@kst.uniza.sk>
2021-10-27 09:41:56 +0200

    04a lopticka sa odraza

--- pong/Lopta.java
+++ pong/Lopta.java
@@ -1,5 +1,8 @@
 public class Lopta {
     private Kruh zobrazenie;
+    private int poziciaY;
+    private int smerX;
+    private int smerY;
     
     public Lopta() {
         this.zobrazenie = new Kruh();
@@ -7,12 +10,21 @@ public class Lopta {
         this.zobrazenie.posunVodorovne(125);
         this.zobrazenie.posunZvisle(85);
         this.zobrazenie.zobraz();
+        this.poziciaY = 150;
+        this.smerX = 5;
+        this.smerY = -5;
     }
     
     public void posun() {
         this.zobrazenie.skry();
-        this.zobrazenie.posunVodorovne(5);
-        this.zobrazenie.posunZvisle(-5);
+        this.zobrazenie.posunVodorovne(this.smerX);
+        this.zobrazenie.posunZvisle(this.smerY);
         this.zobrazenie.zobraz();
+        //this.poziciaY = this.poziciaY - 5;
+        this.poziciaY += this.smerY;
+        
+        if (this.poziciaY == 5 || this.poziciaY == 295) {
+            this.smerY = -this.smerY;
+        }
     }
 }

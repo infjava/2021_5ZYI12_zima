1921cb71d627e18ff89641c99eb3565a107ae6ca
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-10 09:16:57 +0100

    07a - vklady na ucet a vybery z uctu

--- banka/Ucet.java
+++ banka/Ucet.java
@@ -8,4 +8,16 @@ public class Ucet {
         this.vlastnik = vlastnik;
         this.cisloUctu = cisloUctu;
     }
+    
+    public double getSuma() {
+        return this.sumaVCentoch / 100.0;
+    }
+    
+    public void vlozPeniaze(double suma) {
+        this.sumaVCentoch += suma * 100;
+    }
+    
+    public void vyberPeniaze(double suma) {
+        this.sumaVCentoch -= suma * 100;
+    }
 }

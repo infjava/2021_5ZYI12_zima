d93d9d5380c83abcf15c1d568c29d0d1cd66efbb
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-10 08:46:30 +0100

    07a - prazdne triedy

---
+++ banka/Ucet.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Ucet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ucet {
+    /**
+     * Constructor for objects of class Ucet
+     */
+    public Ucet() {
+        // initialise instance variables
+    }
+}

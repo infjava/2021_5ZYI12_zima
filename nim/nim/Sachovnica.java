a558590bae1695039e58a480b9fcde2922f7da72
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-24 09:26:00 +0100

    09a - "sachovnica"

--- nim/nim/Sachovnica.java
+++ nim/nim/Sachovnica.java
@@ -1,5 +1,13 @@
 public class Sachovnica {
     public Sachovnica(int sirka, int vyska) {
-        // initialise instance variables
+        for (int riadok = 0; riadok < vyska; riadok++) {
+            for (int stlpec = 0; stlpec < sirka; stlpec++) {
+                Stvorec policko = new Stvorec();
+                policko.zmenStranu(15);
+                policko.posunVodorovne(15 * stlpec - 60);
+                policko.posunZvisle(15 * riadok - 50);
+                policko.zobraz();
+            }
+        }
     }
 }

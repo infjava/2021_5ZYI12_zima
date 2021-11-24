8d49a25e0570002486d9b149d16ebd61a218962d
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-24 09:26:19 +0100

    09a - striedanie bielych/ciernych policok

--- nim/nim/Sachovnica.java
+++ nim/nim/Sachovnica.java
@@ -1,12 +1,19 @@
 public class Sachovnica {
     public Sachovnica(int sirka, int vyska) {
+        boolean posledneCierne = true;
         for (int riadok = 0; riadok < vyska; riadok++) {
             for (int stlpec = 0; stlpec < sirka; stlpec++) {
                 Stvorec policko = new Stvorec();
                 policko.zmenStranu(15);
                 policko.posunVodorovne(15 * stlpec - 60);
                 policko.posunZvisle(15 * riadok - 50);
-                policko.zobraz();
+                if (!posledneCierne) {
+                    policko.zobraz();
+                }
+                posledneCierne = !posledneCierne;
+            }
+            if (sirka % 2 == 0) {
+                posledneCierne = !posledneCierne;
             }
         }
     }

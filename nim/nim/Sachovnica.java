f9c5aa7fd48ad3ffcb1f8b1a3a4989a6f3fa54cd
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-24 09:32:22 +0100

    09a - iny vypocet ciernych policok

--- nim/nim/Sachovnica.java
+++ nim/nim/Sachovnica.java
@@ -1,19 +1,15 @@
 public class Sachovnica {
     public Sachovnica(int sirka, int vyska) {
-        boolean posledneCierne = true;
         for (int riadok = 0; riadok < vyska; riadok++) {
             for (int stlpec = 0; stlpec < sirka; stlpec++) {
                 Stvorec policko = new Stvorec();
                 policko.zmenStranu(15);
                 policko.posunVodorovne(15 * stlpec - 60);
                 policko.posunZvisle(15 * riadok - 50);
-                if (!posledneCierne) {
+                // ak je riadok + stlpec parny, policko je cierne
+                if ((riadok + stlpec) % 2 == 0) {
                     policko.zobraz();
                 }
-                posledneCierne = !posledneCierne;
-            }
-            if (sirka % 2 == 0) {
-                posledneCierne = !posledneCierne;
             }
         }
     }

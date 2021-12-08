5e6fe5a2eb7dfbd7476f5ce36c7a6ee1b2d07595
Jan Janech <jan.janech@kst.uniza.sk>
2021-12-08 09:42:20 +0100

    11a - zasvecovanie/zhasinanie pixelov na displeji

--- tetris/tetris/Displej.java
+++ tetris/tetris/Displej.java
@@ -3,7 +3,11 @@ public class Displej {
     private static final int SIRKA = 10;
     private static final int VELKOST_PIXELU = 10;
     
+    private final Stvorec[][] pixely;
+    
     public Displej() {
+        this.pixely = new Stvorec[Displej.VYSKA][Displej.SIRKA];
+        
         for (int y = 0; y < Displej.VYSKA; y++) {
             for (int x = 0; x < Displej.SIRKA; x++) {
                 Stvorec pixel = new Stvorec();
@@ -12,11 +16,17 @@ public class Displej {
                 pixel.zmenStranu(Displej.VELKOST_PIXELU - 1);
                 pixel.zmenFarbu("black");
                 pixel.zobraz();
+                
+                this.pixely[y][x] = pixel;
             }
         }
     }
     
     public void nastav(int x, int y, boolean svieti) {
-        
+        if (svieti) {
+            this.pixely[y][x].zmenFarbu("yellow");
+        } else {
+            this.pixely[y][x].zmenFarbu("black");
+        }
     }
 }

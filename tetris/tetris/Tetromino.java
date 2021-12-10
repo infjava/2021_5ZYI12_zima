5407487d2ecb2a217024b9d12d35960de1156235
Jan Janech <jan.janech@kst.uniza.sk>
2021-12-10 09:41:07 +0100

    11b - posun tetromina v 3 moznych smeroch

--- tetris/tetris/Tetromino.java
+++ tetris/tetris/Tetromino.java
@@ -1,11 +1,46 @@
 public class Tetromino {
+    private int poziciaX;
+    private int poziciaY;
+    private boolean[][] tvar;
+    
     public Tetromino(boolean[][] tvar) {
-        int poziciaX = (Displej.SIRKA - tvar[0].length) / 2;
-        int poziciaY = 0;
+        this.poziciaX = (Displej.SIRKA - tvar[0].length) / 2;
+        this.poziciaY = 0;
+        this.tvar = tvar;
         
-        for (int y = 0; y < tvar.length; y++) {
-            for (int x = 0; x < tvar[y].length; x++) {
-                Displej.getInstancia().nastav(x + poziciaX, y + poziciaY, tvar[y][x]);
+        this.zobraz();
+    }
+    
+    public void posunDole() {
+        this.skry();
+        this.poziciaY++;
+        this.zobraz();
+    }
+    
+    public void posunVlavo() {
+        this.skry();
+        this.poziciaX--;
+        this.zobraz();
+    }
+    
+    public void posunVpravo() {
+        this.skry();
+        this.poziciaX++;
+        this.zobraz();
+    }
+    
+    private void skry() {
+        for (int y = 0; y < this.tvar.length; y++) {
+            for (int x = 0; x < this.tvar[y].length; x++) {
+                Displej.getInstancia().nastav(x + this.poziciaX, y + this.poziciaY, false);
+            }
+        }
+    }
+    
+    private void zobraz() {
+        for (int y = 0; y < this.tvar.length; y++) {
+            for (int x = 0; x < this.tvar[y].length; x++) {
+                Displej.getInstancia().nastav(x + this.poziciaX, y + this.poziciaY, this.tvar[y][x]);
             }
         }
     }
